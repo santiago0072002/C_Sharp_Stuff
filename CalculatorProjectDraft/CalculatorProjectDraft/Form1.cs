d64cc71a77b677f97a048e053e946d4872723707
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2020-12-04 11:42:15 -0500

    Add files via upload

---
+++ CalculatorProjectDraft/CalculatorProjectDraft/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorProjectDraft
+{
+    public partial class Form1 : Form
+    {
+        const string divideByZero = "Err!";
+        const string syntaxErr = "SYNTAX ERROR!";
+        bool decimalPointActive = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDisplay.Text)) return;
+            Clipboard.SetText(txtDisplay.Text); 
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            decimalPointActive = false;
+            PreCheck_ButtonClick();
+            previousOperation = Operation.None;
+            txtDisplay.Clear();
+        }
+
+        private void PreCheck_ButtonClick()
+        {
+            if (txtDisplay.Text == divideByZero || txtDisplay.Text == syntaxErr)
+                txtDisplay.Clear();
+            if (previousOperation != Operation.None)
+            {
+                EnableOperatorButtons();
+            }
+        }
+
+        private void EnableOperatorButtons(bool enable = true)
+        {
+            btnMul.Enabled = enable;
+            btnDiv.Enabled = enable;
+            btnAdd.Enabled = enable;
+            if (!enable)
+            {
+                decimalPointActive = false;
+            }
+           
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            decimalPointActive = false;
+            PreCheck_ButtonClick();
+            if (txtDisplay.Text.Length > 0)
+            {
+                double d;
+                if (!double.TryParse(txtDisplay.Text[txtDisplay.Text.Length - 1].ToString(), out d))
+                {
+                    previousOperation = Operation.None;
+                }
+
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+            }
+            if (txtDisplay.Text.Length == 0)
+            {
+                previousOperation = Operation.None;
+            }
+            if (previousOperation != Operation.None)
+            {
+                currentOperation = previousOperation;
+            }
+        }
+
+
+
+
+        private void BtnDiv_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.TextLength == 0) return;
+            PreCheck_ButtonClick();
+            currentOperation = Operation.Div;
+            PerformCalculation(previousOperation);
+
+            previousOperation = currentOperation;
+            EnableOperatorButtons(false);
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void BtnMul_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.TextLength == 0) return;
+            PreCheck_ButtonClick();
+            currentOperation = Operation.Mul;
+            PerformCalculation(previousOperation);
+            previousOperation = currentOperation;
+            EnableOperatorButtons(false);
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void BtnSub_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.TextLength == 0 || previousOperation == Operation.Sub) return;
+            PreCheck_ButtonClick();
+            currentOperation = Operation.Sub;
+            PerformCalculation(previousOperation);
+
+            previousOperation = currentOperation;
+            EnableOperatorButtons(false);
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.TextLength == 0) return;
+            PreCheck_ButtonClick();
+
+            currentOperation = Operation.Add;
+            PerformCalculation(previousOperation);
+
+            previousOperation = currentOperation;
+            EnableOperatorButtons(false);
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void PerformCalculation(Operation previousOperation)
+        {
+            try
+            {
+                if (previousOperation == Operation.None)
+                    return;
+                List<double> lstNums = null;
+
+                switch (previousOperation)
+                {
+                    case Operation.Add:
+                        if (currentOperation == Operation.Sub)
+                        {
+                            currentOperation = Operation.Add;
+                            return;
+                        }
+                        lstNums = txtDisplay.Text.Split('+').Select(double.Parse).ToList();
+                        txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                        break;
+                    case Operation.Sub:
+                        int idx = txtDisplay.Text.LastIndexOf('-'); // To handle ex: -9-2
+                        if (idx > 0)
+                        {
+                            var op1 = Convert.ToDouble(txtDisplay.Text.Substring(0, idx));
+                            var op2 = Convert.ToDouble(txtDisplay.Text.Substring(idx + 1));
+                            txtDisplay.Text = (op1 - op2).ToString();
+                        }
+                        break;
+                    case Operation.Mul:
+                        if (currentOperation == Operation.Sub)
+                        {
+                            currentOperation = Operation.Mul;
+                            return;
+                        }
+                        lstNums = txtDisplay.Text.Split('*').Select(double.Parse).ToList();
+                        txtDisplay.Text = (lstNums[0] * lstNums[1]).ToString();
+                        break;
+                    case Operation.Div:
+                        if (currentOperation == Operation.Sub)
+                        {
+                            currentOperation = Operation.Div;
+                            return;
+                        }
+                        try
+                        {
+                            lstNums = txtDisplay.Text.Split('/').Select(double.Parse).ToList();
+                            if (lstNums[1] == 0)
+                            {
+                                throw new DivideByZeroException();
+                            }
+                            txtDisplay.Text = (lstNums[0] / lstNums[1]).ToString();
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            txtDisplay.Text = divideByZero;
+                        }
+                        break;
+                    case Operation.None:
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                txtDisplay.Text = syntaxErr;
+            }
+        }
+
+        enum Operation
+        {
+            Add, 
+            Sub, 
+            Mul, 
+            Div,
+            None
+        }
+
+        Operation previousOperation = Operation.None;
+        Operation currentOperation = Operation.None;
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn1 as Button).Text;
+        }
+
+        private void BtnNum_Click(Object btn, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn as Button).Text;
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn2 as Button).Text;
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn3 as Button).Text;
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn4 as Button).Text;
+        }
+
+        private void Btn5_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn5 as Button).Text;
+        }
+
+        private void Btn6_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn6 as Button).Text;
+        }
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn7 as Button).Text;
+        }
+
+        private void Btn8_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn8 as Button).Text;
+        }
+
+        private void Btn9_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn9 as Button).Text;
+        }
+
+
+        private void Btn0_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+            {
+                txtDisplay.Text = string.Empty;
+            }
+            EnableOperatorButtons();
+            PreCheck_ButtonClick();
+            txtDisplay.Text += (btn0 as Button).Text;
+        }
+
+        private void BtnRes_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.TextLength == 0) return;
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.None;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // SANTIAGO
+        }
+
+
+        private void btnDecimal_Click_1(object sender, EventArgs e)
+        {
+            {
+                if (decimalPointActive) return;
+                if (txtDisplay.Text == syntaxErr || txtDisplay.Text == divideByZero)
+                {
+                    txtDisplay.Text = string.Empty;
+                }
+                EnableOperatorButtons();
+                PreCheck_ButtonClick();
+                txtDisplay.Text += (sender as Button).Text;
+                decimalPointActive = true;
+            }
+        }
+    }
+}
